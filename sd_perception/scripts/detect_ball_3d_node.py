cf71fb56a9a71112ae25eacb5fabe6e1abae8122
sxj754 <sxj754@case.edu>
2023-06-12 16:11:01 +0900

    Add ball detecting node

---
+++ sd_perception/scripts/detect_ball_3d_node.py
@@ -0,0 +1,88 @@
+# Copyright 2023 Josh Newans
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rospy
+from geometry_msgs.msg      import Point
+from visualization_msgs.msg import Marker
+import math
+
+class DetectBall3d():
+
+    def __init__(self):
+        rospy.loginfo('Detecting in 3D')
+
+        self.ball2d_sub = rospy.Subscriber("/detect_ball/detected_ball", Point, self.ball_rcv_callback)
+        self.ball3d_pub = rospy.Publisher("/detect_ball/detected_ball_3d", Point, queue_size=1)
+        self.ball_marker_pub = rospy.Publisher("/detect_ball/ball_3d_marker", Marker, queue_size=1)
+
+        rospy.set_param("h_fov",1.089)
+        rospy.set_param("ball_radius",0.033)
+        rospy.set_param("aspect_ratio",4.0/3.0)
+        rospy.set_param("camera_frame",'front_cam_link')
+
+        self.h_fov = rospy.get_param('h_fov')
+        self.v_fov = self.h_fov/rospy.get_param('aspect_ratio')
+        self.ball_radius = rospy.get_param('ball_radius')
+        self.camera_frame = rospy.get_param('camera_frame')
+
+
+    def ball_rcv_callback(self, data:Point):
+
+        # Calculate angular size and consequently distance
+        ang_size = data.z*self.h_fov
+        d = self.ball_radius/(math.atan(ang_size/2))
+
+        # Calculate angular and distance deviations in X and Y
+        y_ang = data.y*self.v_fov/2
+        y = d*math.sin(y_ang)
+        d_proj = d*math.cos(y_ang)
+
+        x_ang = data.x*self.h_fov/2
+        x = d_proj*math.sin(x_ang)
+        z = d_proj*math.cos(x_ang)
+
+
+        p = Point()
+        p.x = x
+        p.y = y
+        p.z = z
+        self.ball3d_pub.publish(p)
+
+
+        m = Marker()
+        m.header.frame_id = self.camera_frame
+
+        m.id = 0
+        m.type = Marker.SPHERE
+        m.action = Marker.ADD
+        m.pose.position.x = x
+        m.pose.position.y = y
+        m.pose.position.z = z
+        m.scale.x = self.ball_radius*2
+        m.scale.y = self.ball_radius*2
+        m.scale.z = self.ball_radius*2
+        m.color.r = 0.933
+        m.color.g = 1.0
+        m.color.b = 0.0
+        m.color.a = 1.0
+
+        self.ball_marker_pub.publish(m)
+        print(m.pose.position)
+
+
+if __name__ == '__main__':
+    rospy.init_node('detect_ball_3d_node', anonymous=True)
+    DetectBall3d()
+    rospy.spin()
+
---
+++ sd_perception/scripts/detect_ball_node.py
@@ -0,0 +1,308 @@
+# Copyright 2023 Josh Newans
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rospy
+import cv2
+import numpy as np;
+from sensor_msgs.msg        import Image
+from geometry_msgs.msg      import Point
+from cv_bridge              import CvBridge, CvBridgeError
+
+
+def find_circles(image, tuning_params):
+
+    blur = 5
+
+    x_min   = tuning_params["x_min"]
+    x_max   = tuning_params["x_max"]
+    y_min   = tuning_params["y_min"]
+    y_max   = tuning_params["y_max"]
+    
+    search_window = [x_min, y_min, x_max, y_max]
+
+    working_image    = cv2.blur(image, (blur, blur))
+
+    #- Blur image to remove noise
+    # if blur > 0: 
+    #     image    = cv2.blur(image, (blur, blur))
+    #     #- Show result
+    #     if imshow:
+    #         cv2.imshow("Blur", image)
+    #         cv2.waitKey(0)
+        
+    #- Search window
+    if search_window is None: search_window = [0.0, 0.0, 1.0, 1.0]
+    search_window_px = convert_rect_perc_to_pixels(search_window, image)
+    
+    
+    #- Convert image from BGR to HSV
+    working_image     = cv2.cvtColor(working_image, cv2.COLOR_BGR2HSV)    
+    
+    #- Apply HSV threshold
+    thresh_min = (tuning_params["h_min"], tuning_params["s_min"], tuning_params["v_min"])
+    thresh_max = (tuning_params["h_max"], tuning_params["s_max"], tuning_params["v_max"])
+    working_image    = cv2.inRange(working_image, thresh_min, thresh_max)
+
+
+    # Dilate and Erode
+    working_image = cv2.dilate(working_image, None, iterations=2)
+    working_image = cv2.erode(working_image, None, iterations=2)
+    
+
+    # Make a copy of the image for tuning
+    tuning_image = cv2.bitwise_and(image,image,mask = working_image)
+
+    # Apply the search window
+    working_image = apply_search_window(working_image, search_window)
+
+    # Invert the image to suit the blob detector
+    working_image = 255-working_image
+
+
+
+    # Set up the SimpleBlobdetector with default parameters.
+    params = cv2.SimpleBlobDetector_Params()
+        
+    # Change thresholds
+    params.minThreshold = 0
+    params.maxThreshold = 100
+        
+    # Filter by Area.
+    params.filterByArea = True
+    params.minArea = 30
+    params.maxArea = 20000
+        
+    # Filter by Circularity
+    params.filterByCircularity = True
+    params.minCircularity = 0.1
+        
+    # Filter by Convexity
+    params.filterByConvexity = True
+    params.minConvexity = 0.5
+        
+    # Filter by Inertia
+    params.filterByInertia =True
+    params.minInertiaRatio = 0.5
+
+    detector = cv2.SimpleBlobDetector_create(params)
+
+    # Run detection!
+    keypoints = detector.detect(working_image)
+
+    size_min_px = tuning_params['sz_min']*working_image.shape[1]/100.0
+    size_max_px = tuning_params['sz_max']*working_image.shape[1]/100.0
+
+    keypoints = [k for k in keypoints if k.size > size_min_px and k.size < size_max_px]
+
+    
+    # Set up main output image
+    line_color=(0,0,255)
+
+    out_image = cv2.drawKeypoints(image, keypoints, np.array([]), line_color, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    out_image = draw_window2(out_image, search_window_px)
+
+    # Set up tuning output image
+    
+    tuning_image = cv2.drawKeypoints(tuning_image, keypoints, np.array([]), line_color, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # tuning_image = draw_window(tuning_image, search_window)
+    # cv2.rectangle(image,(x_min_px,y_min_px),(x_max_px,y_max_px),color,line)
+    tuning_image = draw_window2(tuning_image, search_window_px)
+
+
+    keypoints_normalised = [normalise_keypoint(working_image, k) for k in keypoints]
+
+    return keypoints_normalised, out_image, tuning_image
+
+
+#---------- Apply search window: returns the image
+#-- return(image)
+def apply_search_window(image, window_adim=[0.0, 0.0, 1.0, 1.0]):
+    rows = image.shape[0]
+    cols = image.shape[1]
+    x_min_px    = int(cols*window_adim[0]/100)
+    y_min_px    = int(rows*window_adim[1]/100)
+    x_max_px    = int(cols*window_adim[2]/100)
+    y_max_px    = int(rows*window_adim[3]/100)    
+    
+    #--- Initialize the mask as a black image
+    mask = np.zeros(image.shape,np.uint8)
+    
+    #--- Copy the pixels from the original image corresponding to the window
+    mask[y_min_px:y_max_px,x_min_px:x_max_px] = image[y_min_px:y_max_px,x_min_px:x_max_px]   
+    
+    #--- return the mask
+    return(mask)
+
+
+#---------- Draw search window: returns the image
+#-- return(image)
+def draw_window2(image,              #- Input image
+                rect_px,        #- window in adimensional units
+                color=(255,0,0),    #- line's color
+                line=5,             #- line's thickness
+               ):
+    
+    #-- Draw a rectangle from top left to bottom right corner
+
+    return cv2.rectangle(image,(rect_px[0],rect_px[1]),(rect_px[2],rect_px[3]),color,line)
+
+
+def convert_rect_perc_to_pixels(rect_perc, image):
+    rows = image.shape[0]
+    cols = image.shape[1]
+
+    scale = [cols, rows, cols, rows]
+
+    
+    # x_min_px    = int(cols*window_adim[0])
+    # y_min_px    = int(rows*window_adim[1])
+    # x_max_px    = int(cols*window_adim[2])
+    # y_max_px    = int(rows*window_adim[3]) 
+    return [int(a*b/100) for a,b in zip(rect_perc, scale)]
+
+
+def normalise_keypoint(cv_image, kp):
+    rows = float(cv_image.shape[0])
+    cols = float(cv_image.shape[1])
+    # print(rows, cols)
+    center_x    = 0.5*cols
+    center_y    = 0.5*rows
+    # print(center_x)
+    x = (kp.pt[0] - center_x)/(center_x)
+    y = (kp.pt[1] - center_y)/(center_y)
+    return cv2.KeyPoint(x, y, kp.size/cv_image.shape[1])
+
+
+def create_tuning_window(initial_values):
+    cv2.namedWindow("Tuning", 0)
+    cv2.createTrackbar("x_min","Tuning",initial_values['x_min'],100,no_op)
+    cv2.createTrackbar("x_max","Tuning",initial_values['x_max'],100,no_op)
+    cv2.createTrackbar("y_min","Tuning",initial_values['y_min'],100,no_op)
+    cv2.createTrackbar("y_max","Tuning",initial_values['y_max'],100,no_op)
+    cv2.createTrackbar("h_min","Tuning",initial_values['h_min'],180,no_op)
+    cv2.createTrackbar("h_max","Tuning",initial_values['h_max'],180,no_op)
+    cv2.createTrackbar("s_min","Tuning",initial_values['s_min'],255,no_op)
+    cv2.createTrackbar("s_max","Tuning",initial_values['s_max'],255,no_op)
+    cv2.createTrackbar("v_min","Tuning",initial_values['v_min'],255,no_op)
+    cv2.createTrackbar("v_max","Tuning",initial_values['v_max'],255,no_op)
+    cv2.createTrackbar("sz_min","Tuning",initial_values['sz_min'],100,no_op)
+    cv2.createTrackbar("sz_max","Tuning",initial_values['sz_max'],100,no_op)
+
+
+def get_tuning_params():
+    trackbar_names = ["x_min","x_max","y_min","y_max","h_min","h_max","s_min","s_max","v_min","v_max","sz_min","sz_max"]
+    return {key:cv2.getTrackbarPos(key, "Tuning") for key in trackbar_names}
+
+
+def wait_on_gui():
+    cv2.waitKey(2)
+
+
+def no_op(x):
+    pass
+
+
+class DetectBall():
+
+    def __init__(self):
+        rospy.loginfo('Looking for the ball...')
+
+        rospy.set_param('~tuning_mode', False) # ~ since it is a private param (it is put inside a node)
+        rospy.set_param('x_min', 0)
+        rospy.set_param('x_max', 100)
+        rospy.set_param('y_min', 32)
+        rospy.set_param('y_max', 100)
+        rospy.set_param("h_min", 23)
+        rospy.set_param("h_max", 57)
+        rospy.set_param("s_min", 0)
+        rospy.set_param("s_max", 255)
+        rospy.set_param("v_min", 0)
+        rospy.set_param("v_max", 255)
+        rospy.set_param("sz_min", 0)
+        rospy.set_param("sz_max", 100)
+        
+        self.tuning_mode = rospy.get_param('~tuning_mode')
+        self.tuning_params = {
+            'x_min': rospy.get_param('x_min'),
+            'x_max': rospy.get_param('x_max'),
+            'y_min': rospy.get_param('y_min'),
+            'y_max': rospy.get_param('y_max'),
+            'h_min': rospy.get_param('h_min'),
+            'h_max': rospy.get_param('h_max'),
+            's_min': rospy.get_param('s_min'),
+            's_max': rospy.get_param('s_max'),
+            'v_min': rospy.get_param('v_min'),
+            'v_max': rospy.get_param('v_max'),
+            'sz_min': rospy.get_param('sz_min'),
+            'sz_max': rospy.get_param('sz_max')
+        }
+
+        self.bridge = CvBridge()
+
+        if (self.tuning_mode):
+            create_tuning_window(self.tuning_params)
+
+        self.image_sub = rospy.Subscriber("/front_cam/image_raw", Image, self.callback)
+        self.image_out_pub = rospy.Publisher("/detect_ball/image_out", Image, queue_size=1)
+        self.image_tuning_pub = rospy.Publisher("/detect_ball/image_tuning", Image, queue_size=1)
+        self.ball_pub = rospy.Publisher("/detect_ball/detected_ball", Point, queue_size=1)
+
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        try:
+            if (self.tuning_mode):
+                self.tuning_params = get_tuning_params()
+
+            keypoints_norm, out_image, tuning_image = find_circles(cv_image, self.tuning_params)
+
+            img_to_pub = self.bridge.cv2_to_imgmsg(out_image, "bgr8")
+            img_to_pub.header = data.header
+            self.image_out_pub.publish(img_to_pub)
+
+            img_to_pub = self.bridge.cv2_to_imgmsg(tuning_image, "bgr8")
+            img_to_pub.header = data.header
+            self.image_tuning_pub.publish(img_to_pub)
+
+            point_out = Point()
+
+            # Keep the biggest point
+            # They are already converted to normalised coordinates
+            for i, kp in enumerate(keypoints_norm):
+                x = kp.pt[0]
+                y = kp.pt[1]
+                s = kp.size
+
+                rospy.loginfo(f"Pt {i}: ({x},{y},{s})")
+
+                if (s > point_out.z):                    
+                    point_out.x = x
+                    point_out.y = y
+                    point_out.z = s
+
+            if (point_out.z > 0):
+                self.ball_pub.publish(point_out) 
+        except CvBridgeError as e:
+            print(e)  
+
+
+if __name__ == '__main__':
+    rospy.init_node('detect_ball_node', anonymous=True)
+    DetectBall()
+    rospy.spin()
+    # while not rospy.is_shutdown():
+    #     wait_on_gui()
